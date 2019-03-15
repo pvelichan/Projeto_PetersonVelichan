b38a810619f1604ba8d860930985ea43a2e8bd28
pvelichan <47905471+pvelichan@users.noreply.github.com>
2019-03-14 22:20:37 -0300

    Add files via upload

---
+++ src/main/java/aplicacao_pvelichan/java/DisplayMessageActivity.java
@@ -0,0 +1,22 @@
+package aplicacao_pvelichan.java;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(aplicacao_pvelichan.myfirstapp.MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+
+    }
+}
