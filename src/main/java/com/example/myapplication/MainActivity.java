2a17dd9e1798eac6b659dce6dfc149cff64130d6
pvelichan <47905471+pvelichan@users.noreply.github.com>
2019-03-21 22:43:29 -0300

    Add files via upload

---
+++ src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btLogin = (Button) findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+                TextView tLogin = (TextView) findViewById(R.id.tLogin);
+                TextView tSenha = (TextView) findViewById(R.id.tSenha);
+                String login = tLogin.getText().toString();
+                String senha = tSenha.getText().toString();
+                if (login.equals("Pedro") && senha.equals("123")) {
+                    alert("Login realizado com sucesso");
+
+                } else {
+                    alert("Login ou senha incorretos");
+                }
+            }
+        });
+    }
+
+    private void alert(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+
+    }
+}
